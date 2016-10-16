46686cfea472e1eda4686445671fc5b055442e86
Gama Aguilar <gama@9104studios.com>
2016-10-16 16:09:30 -0700

    75% completed

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -3,7 +3,7 @@
         var ref = firebase.database().ref().child("rooms");
         var rooms = $firebaseArray(ref);
         //creating room objects with AngularFire
-        rooms.$add({ chatRoomName: "Chat Room Name"}).then(function(ref){
+        rooms.$add({ chatRoomName: "Chat Room Name" }).then(function(ref){
             var id = ref.key;
             console.log("added record with id " + id);
             list.$indexFor(id); //returns location in the array
--- dist/scripts/services/Room.js
+++ dist/scripts/services/Room.js
@@ -2,7 +2,12 @@
     function Room ($firebaseArray) {
         var ref = firebase.database().ref().child("rooms");
         var rooms = $firebaseArray(ref);
-        
+        //creating room objects with AngularFire
+        rooms.$add({ chatRoomName: "Chat Room Name" }).then(function(ref){
+            var id = ref.key;
+            console.log("added record with id " + id);
+            list.$indexFor(id); //returns location in the array
+        });
         return {
             all: rooms
         };

13c5c366282fbdd054e8a049834312dca07d536b
Gama Aguilar <gama@9104studios.com>
2016-11-07 22:33:34 -0800

    99% sure checkpoint 5 username completed - need to validate that the username is being saved to the appropriate chookie.

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -12,9 +12,21 @@
                 controller: 'HomeCtrl as home',
                 templateUrl: '/templates/home.html'
         });
+    }         
+            
+    
+    function BlocChatCookies ($cookies) {
+        var currentUser = $cookies.get('blocChatCurrentUser');
+        while (!currentUser || currentUser === '') {
+            currentUser = prompt("Set a username!  This name will appear when you send a message.  Sorry, not choice but to set a username:");  
+            console.log(currentUser);
+        }
+        
+        return currentUser;
     }
     
     angular
-        .module('blocChat', ['ui.router', 'ui.bootstrap', 'firebase'])
-        .config(config);
+        .module('blocChat', ['ui.router', 'ui.bootstrap', 'firebase', 'ngCookies'])
+        .config(config)
+        .run(['$cookies', BlocChatCookies]);
 })();
